0f60857f3780a6b9c47cd3137b3d2b900b74c2d4
AndrejLapin <lapin.andrej1@gmail.com>
2022-03-30 23:20:28 +0300

    added path node editor script, moved some functionality from PathTile to path editor

--- BlackSmithGame/Assets/Scripts/PathNode.cs
+++ BlackSmithGame/Assets/Scripts/PathNode.cs
@@ -8,12 +8,19 @@ public class PathNode : MonoBehaviour
     static Color oragne = new Color(1f, 0.647f, 0f, 0.5f);
     static Color red = new Color(1f, 0f, 0f, 1f);
 
-    [SerializeField] Vector2 topLeft = new Vector2(-0.5f, 0.5f);
-    [SerializeField] Vector2 topRight = new Vector2(0.5f, 0.5f);
-    [SerializeField] Vector2 bottomLeft = new Vector2(-0.5f, -0.5f);
-    [SerializeField] Vector2 bottomRight = new Vector2(0.5f, -0.5f);
+    [SerializeField] Vector3 topLeft = new Vector3(-0.5f, 0, 0.5f);
+    [SerializeField] Vector3 topRight = new Vector3(0.5f, 0, 0.5f);
+    [SerializeField] Vector3 bottomLeft = new Vector3(-0.5f, 0, -0.5f);
+    [SerializeField] Vector3 bottomRight = new Vector3(0.5f, 0, -0.5f);
 
-    Vector3 up = new Vector3(0, 0, 90);
+    // should probably be public fields
+    // or just setter methods
+    // we need to be able to assign neighbours after one isss assigned in another neghbour
+    // should work during editor runtime
+    [SerializeField] PathNode topNeighbour;
+    [SerializeField] PathNode bottomNeighbour;
+    [SerializeField] PathNode leftNeighbour;
+    [SerializeField] PathNode rightNeighbour;
 
     // Start is called before the first frame update
     void Start()
@@ -29,27 +36,48 @@ public class PathNode : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = red;
+        // Gizmos.color = red;
 
-        Vector3 topLeftPos = new Vector3(topLeft.x + transform.position.x, transform.position.y, topLeft.y + transform.position.z);
-        Vector3 topRightPos = new Vector3(topRight.x + transform.position.x, transform.position.y, topRight.y + transform.position.z);
-        Vector3 bottomLeftPos = new Vector3(bottomLeft.x + transform.position.x, transform.position.y, bottomLeft.y + transform.position.z);
-        Vector3 bottomRightPos = new Vector3(bottomRight.x + transform.position.x, transform.position.y, bottomRight.y + transform.position.z);
+        // Vector3 topLeftPos = new Vector3(topLeft.x + transform.position.x, topLeft.y + transform.position.y, topLeft.z + transform.position.z);
+        // Vector3 topRightPos = new Vector3(topRight.x + transform.position.x, topRight.y + transform.position.y, topRight.z + transform.position.z);
+        // Vector3 bottomLeftPos = new Vector3(bottomLeft.x + transform.position.x, bottomLeft.y + transform.position.y, bottomLeft.z + transform.position.z);
+        // Vector3 bottomRightPos = new Vector3(bottomRight.x + transform.position.x, bottomRight.y + transform.position.y, bottomRight.z + transform.position.z);
 
-        Gizmos.DrawLine(topLeftPos, topRightPos);
-        Gizmos.DrawLine(topLeftPos, bottomLeftPos);
-        Gizmos.DrawLine(topRightPos, bottomRightPos);
-        Gizmos.DrawLine(bottomLeftPos, bottomRightPos);
+        // Gizmos.DrawLine(topLeftPos, topRightPos);
+        // Gizmos.DrawLine(topLeftPos, bottomLeftPos);
+        // Gizmos.DrawLine(bottomLeftPos, topRightPos);
+        // Gizmos.DrawLine(topRightPos, bottomRightPos);
+        // Gizmos.DrawLine(bottomLeftPos, bottomRightPos);
 
-        Gizmos.color = oragne;
+        // Gizmos.color = oragne;
 
-        Mesh someMesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = someMesh;
+        // Mesh someMesh = new Mesh();
+        // GetComponent<MeshFilter>().mesh = someMesh;
 
-        someMesh.vertices = new Vector3[] {topLeftPos, topRightPos, bottomLeftPos, bottomRightPos};
-        someMesh.triangles = new int[] {0, 1, 2, 2, 1, 3};
-        someMesh.RecalculateNormals();
+        // someMesh.vertices = new Vector3[] {topLeftPos, topRightPos, bottomLeftPos, bottomRightPos};
+        // someMesh.triangles = new int[] {0, 1, 2, 2, 1, 3};
+        // someMesh.RecalculateNormals();
 
-        Gizmos.DrawMesh(someMesh);
+        // Gizmos.DrawMesh(someMesh);
+    }
+
+    public Vector3 GetTopLeft()
+    {
+        return topLeft;
+    }
+
+    public Vector3 GetTopRight()
+    {
+        return topRight;
+    }
+
+    public Vector3 GetBottomLeft()
+    {
+        return bottomLeft;
+    }
+
+    public Vector3 GetBottomRight()
+    {
+        return bottomRight;
     }
 }
---
+++ BlackSmithGame/Assets/Scripts/PathNodeEditor.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class PathNodeEditor : MonoBehaviour
+{
+    // half transparent orange
+    static Color oragne = new Color(1f, 0.647f, 0f, 0.5f);
+    static Color red = new Color(1f, 0f, 0f, 1f);
+
+    // this should be global
+    bool drawGizmos = true;
+    PathNode myNode;
+    Mesh myMesh;
+
+    /* ---- Verticies ----- */
+
+    Vector3 topLeftVertex;
+    Vector3 topRightVertex;
+    Vector3 bottomLeftVertex;
+    Vector3 bottomRightVertex;
+
+    /* -------------------- */
+
+    Vector3 lastTopLeft;
+    Vector3 lastTopRight;
+    Vector3 lastBottomLeft;
+    Vector3 lastBottomRight;
+
+    private void Awake()
+    {
+        // destroy this if its not a debug version
+
+        myNode = GetComponent<PathNode>();
+        myMesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = myMesh;
+
+        AssignVertexOffsets();
+
+        CalculateVerticies();
+
+        UpdateMesh();
+        myMesh.triangles = new int[] {0, 1, 2, 2, 1, 3};
+        myMesh.RecalculateNormals();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EditorApplication.isPlaying)
+        {
+            return;
+        }
+
+        if(lastTopLeft != myNode.GetTopLeft()
+        || lastTopRight != myNode.GetTopRight()
+        || lastBottomLeft != myNode.GetBottomLeft()
+        || lastBottomRight != myNode.GetBottomLeft()) // I guess just move stuff to on gizmo draw and update mesh in OnGizmos draw, seems more efficient
+        {
+            AssignVertexOffsets();
+
+            CalculateVerticies();
+
+            UpdateMesh();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(drawGizmos)
+        {
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawLine(topLeftVertex, topRightVertex);
+            Gizmos.DrawLine(topLeftVertex, bottomLeftVertex);
+            Gizmos.DrawLine(bottomLeftVertex, topRightVertex);
+            Gizmos.DrawLine(topRightVertex, bottomRightVertex);
+            Gizmos.DrawLine(bottomLeftVertex, bottomRightVertex);
+
+            Gizmos.color = oragne;
+
+            Gizmos.DrawMesh(myMesh);
+        }
+    }
+
+    void AssignVertexOffsets()
+    {
+        lastTopLeft = myNode.GetTopLeft();
+        lastTopRight = myNode.GetTopRight();
+        lastBottomLeft = myNode.GetBottomLeft();
+        lastBottomRight = myNode.GetBottomRight();
+    }
+
+    void CalculateVerticies()
+    {
+        topLeftVertex = new Vector3(lastTopLeft.x + transform.position.x, lastTopLeft.y + transform.position.y, lastTopLeft.z + transform.position.z);
+        topRightVertex = new Vector3(lastTopRight.x + transform.position.x, lastTopRight.y + transform.position.y, lastTopRight.z + transform.position.z);
+        bottomLeftVertex = new Vector3(lastBottomLeft.x + transform.position.x, lastBottomLeft.y + transform.position.y, lastBottomLeft.z + transform.position.z);
+        bottomRightVertex = new Vector3(lastBottomRight.x + transform.position.x, lastBottomRight.y + transform.position.y, lastBottomRight.z + transform.position.z);
+    }
+
+    void UpdateMesh()
+    {
+        myMesh.vertices = new Vector3[] {topLeftVertex, topRightVertex, bottomLeftVertex, bottomRightVertex};
+    }
+}
