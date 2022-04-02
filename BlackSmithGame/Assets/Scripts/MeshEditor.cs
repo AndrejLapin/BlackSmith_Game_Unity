e454c23daf7a6b7d28eb36ad7a3d837520ee3713
AndrejLapin <lapin.andrej1@gmail.com>
2022-04-02 21:41:20 +0300

    improved mesh editor

--- BlackSmithGame/Assets/Scripts/MeshEditor.cs
+++ BlackSmithGame/Assets/Scripts/MeshEditor.cs
@@ -6,17 +6,26 @@ using System;
 [ExecuteInEditMode]
 public class MeshEditor : MonoBehaviour
 {
-    Vector3[] originalVerticies;
     [SerializeField] Vector3[] verticies;
     [SerializeField] int[] tris;
     [SerializeField] Vector2[] UV;
     [SerializeField] Vector3[] normals;
     [SerializeField] Vector3 meshScale = new Vector3(1.0f, 1.0f, 1.0f);
-    [SerializeField] bool enableVertexModification = false;
+    Vector3 lasMeshScale;
+    [SerializeField] Vector2 uvOffset = new Vector2(0.0f, 0.0f);
+    Vector2 lastUVOffset;
+    [SerializeField] Vector2 uvScale = new Vector2(1.0f, 1.0f);
+    Vector2 lasUVScale;
     MeshFilter meshFilter = null;
+
+    const float minScaleValue = 0.0000000001f;
 
     private void Awake() 
     {
+        lasMeshScale = meshScale;
+        lasUVScale = uvScale;
+        lastUVOffset = uvOffset;
+
         meshFilter = GetComponent<MeshFilter>();
         if(!meshFilter)
         {
@@ -28,12 +37,10 @@ public class MeshEditor : MonoBehaviour
             gameObject.AddComponent<MeshRenderer>();
         }
 
-        originalVerticies = meshFilter.mesh.vertices;
         verticies = meshFilter.mesh.vertices;
         tris = meshFilter.mesh.triangles;
         UV = meshFilter.mesh.uv;
         normals = meshFilter.mesh.normals;
-        //myMesh = meshFilter.mesh;
     }
 
     // Start is called before the first frame update
@@ -48,35 +55,37 @@ public class MeshEditor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 objectOrigin = gameObject.transform.position;
-
-        if(verticies.Length != originalVerticies.Length)
+        if(meshScale != lasMeshScale)
         {
-            //Array.Clear(originalVerticies, 0, originalVerticies.Length); // do we actually need to clear it?, doubt
-            originalVerticies = new Vector3[verticies.Length];
+            meshScale.x = meshScale.x == 0 ? minScaleValue : meshScale.x;
+            meshScale.y = meshScale.y == 0 ? minScaleValue : meshScale.y;
+            meshScale.z = meshScale.z == 0 ? minScaleValue : meshScale.z;
+
             for(int index = 0; index < verticies.Length; index++)
             {
-                originalVerticies[index] = new Vector3(verticies[index].x / meshScale.x, verticies[index].y / meshScale.y, verticies[index].z / meshScale.z);
+                Vector3 notScaledVertex = new Vector3(verticies[index].x / lasMeshScale.x, verticies[index].y / lasMeshScale.y, verticies[index].z / lasMeshScale.z);
+                verticies[index] = Vector3.Scale(notScaledVertex, meshScale);
+            }
+            lasMeshScale = meshScale;
+        }
+
+        if(uvScale != lasUVScale)
+        {
+            uvScale.x = uvScale.x == 0 ? minScaleValue : uvScale.x;
+            uvScale.y = uvScale.y == 0 ? minScaleValue : uvScale.y;
+            for(int index = 0; index < UV.Length; index++)
+            {
+                UV[index].x *= uvScale.x / lasUVScale.x;
+                UV[index].y *= uvScale.y / lasUVScale.y;
             }
         }
 
-        for(int index = 0; index < verticies.Length; index++)
+        if(uvOffset != lastUVOffset)
         {
-            
-        }
-
-        for(int index = 0; index < verticies.Length; index++)
-        {
-            if(enableVertexModification)
+            for(int index = 0; index < UV.Length; index++)
             {
-                originalVerticies[index] = new Vector3(verticies[index].x / meshScale.x, verticies[index].y / meshScale.y, verticies[index].z / meshScale.z);
-            }
-            else
-            {
-                // multiply by scale
-                Vector3 scaledDistanceVector = Vector3.Scale(originalVerticies[index], meshScale);
-                // apply to vertex position
-                verticies[index] = scaledDistanceVector;// + objectOrigin;
+                UV[index].x += uvOffset.x - lastUVOffset.x;
+                UV[index].y += uvOffset.y - lastUVOffset.y;
             }
         }
 
